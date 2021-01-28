e46b589f3063028dcf3a8cfaac39cf7a40b0a479
matthieu <matthieumailpro@gmail.com>
2021-01-28 20:46:01 +0100

    Amélioration graphique + ajout de github dans le footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import '../styles/Footer.css';
+import GitHub from '../icons/GitHub';
 
 const Footer = () => {
 
     return (
         <footer className="footer">
             <p className="texteFooter">&copy; 2021 - Matthieu MEURILLON. Tous droits réservés.</p>
+            <a href="https://github.com/Littchii" title="mon github" className="icons" target="_blank" rel="noreferrer">
+                <GitHub height={25} width={30} />
+            </a>
         </footer>
     )
 
