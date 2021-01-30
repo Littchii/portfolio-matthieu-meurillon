d888370eb46a6d3c70747ebfd63f16605a1bf208
matthieu <matthieumailpro@gmail.com>
2021-01-30 14:16:22 +0100

    Suppression draft-js

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,20 +1,9 @@
-import {Editor, EditorState} from 'draft-js';
-import { useState } from 'react';
 import '../styles/Admin.css';
 
 const Admin = () => {
-
-    const [editorState, setEditorState] = useState(
-        () => EditorState.createEmpty(),
-    );
-
-    console.log(editorState);
-
+    
     return (
         <div className="adminContainer">
-            <Editor 
-            editorState={editorState} 
-            onChange={setEditorState} />
         </div>
     )
 
