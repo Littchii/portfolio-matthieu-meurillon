ff7e0f869a8381c1213268ad46a12bf0486b90fd
Matthieu Meurillon <matthieumailpro@gmail.com>
2021-02-10 15:37:53 +0100

    Protection admin

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import '../styles/Admin.css';
+import { useHistory } from 'react-router-dom';
 
 const Admin = () => {
+
+    const history = useHistory();
+
+    useEffect(() => {
+        if(! localStorage.getItem('auth')) {
+            history.push('/');
+        }
+    });
     
     return (
         <div className="adminContainer">
