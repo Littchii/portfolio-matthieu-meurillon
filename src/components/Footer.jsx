d7424bd932f88424ddc0f6d6858e3e8d8e42473b
Matthieu <matthieumailpro@gmail.com>
2021-02-14 16:19:54 +0100

    Ajout linkedin dans le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import '../styles/Footer.css';
 import GitHub from '../icons/GitHub';
+import Linkedin from '../icons/Linkedin';
 
 const Footer = () => {
 
@@ -7,7 +8,10 @@ const Footer = () => {
         <footer className="footer">
             <p className="texteFooter">&copy; 2021 - Matthieu MEURILLON. Tous droits réservés.</p>
             <a href="https://github.com/Littchii" title="mon github" className="icons" target="_blank" rel="noreferrer">
-                <GitHub height={25} width={30} />
+                <GitHub height={23} width={28} />
+            </a>
+            <a href="https://www.linkedin.com/in/matthieu-meurillon-468b0218b/" title="mon linkedin" className="icons" target="_blank" rel="noreferrer">
+                <Linkedin height={23} width={28} />
             </a>
         </footer>
     )
